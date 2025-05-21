b0f84839430cf48f573f67898ad5383b6ca23d86
Thomas <francescon.thomas@gmail.com>
2025-05-21 16:24:11 +0200

    fix params

--- app/country/[id]/page.tsx
+++ app/country/[id]/page.tsx
@@ -6,14 +6,8 @@ import { notFound } from "next/navigation"
 import { fetchUserData } from "@/lib/api/request/request"
 import { Country } from "@/lib/types/country"
 
-type Props = {
-  params: {
-    id: string
-  }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
+export default async function CountryPage({params}: {params: { id: string }}) {
 
-export default async function CountryPage({ params }: Props) {
   const countryData = await fetchUserData(params.id) as Country | Country[]
   const country: Country = Array.isArray(countryData) ? countryData[0] : countryData
 
